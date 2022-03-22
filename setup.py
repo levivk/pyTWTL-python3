7dc37cd1f81967674affe9492324293f35187a1f
Levi Vande Kamp <levi.vandekamp@gmail.com>
2022-03-22 17:20:21 -0500

    fixed finding package and added dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="pyTWTL",
     version="1.0",
-    packages=find_packages(),
+    package_dir={"": "."},
+    packages=find_packages(where='.'),
+    python_requires=">=3.8",
+    install_requires=[
+        'pyYAML >= 6.0',
+        'networkx >= 2.7.1'
+    ]
 )
