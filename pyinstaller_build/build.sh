d92efdd45bc810a08081a7ee2ed4f357fd6c62d3
Levi Vande Kamp <vand1718@umn.edu>
2022-04-15 11:53:59 -0500

    Built using Ubuntu 18.04. Build script uses docker.

--- pyinstaller_build/build.sh
+++ pyinstaller_build/build.sh
@@ -3,17 +3,22 @@
 # cd to script dir
 cd $(dirname $0)
 
-# remove dist and run pyinstaller
-rm -rf dist
-pyinstaller twtl_translate.spec
-
 # copy to respective folder
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	# Use docker to run in Ubuntu 18.04
+	docker build -t pytwtl .
+	docker create --name pytwtl-cont pytwtl
 	rm -rf ../pyTWTL/bin-linux/*
-        cp -ra dist/twtl_translate/. ../pyTWTL/bin-linux/
+	docker cp pytwtl-cont:/b/dist/twtl_translate/. ../pyTWTL/bin-linux/
+	docker rm pytwtl-cont
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	rm -rf ../pyTWTL/bin-darwin
-        cp -r dist/twtl_translate ../pyTWTL/bin-darwin
+	# remove dist and run pyinstaller
+	rm -rf dist
+	pyinstaller twtl_translate.spec
+	# Copy to folder
+	rm -rf ../pyTWTL/bin-darwin/*
+        cp -ra dist/twtl_translate/. ../pyTWTL/bin-darwin/
 else
         echo "Unsupported OS type:" "$OSTYPE"
 fi
